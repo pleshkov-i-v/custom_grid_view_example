5dbd251fb28fc07a31aa1f1ad20db4bb6fece967
Ivan Pleshkov <pleshkov.i.v@gmail.com>
2026-01-29 17:19:19 +0300

    add custom grid view implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,280 @@
-import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(const CutomGridViewExampleApp());
+
+class CutomGridViewExampleApp extends StatefulWidget {
+  const CutomGridViewExampleApp({super.key});
+
+  @override
+  State<CutomGridViewExampleApp> createState() =>
+      _CutomGridViewExampleAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _CutomGridViewExampleAppState extends State<CutomGridViewExampleApp> {
+  late final List<int> headers;
+  late List<Object> items;
+  late List<GridViewItemType> gridViewItemsType;
+  late CustomGridDelegate gridDelegate;
+  late ScrollController scrollController;
+  @override
+  void initState() {
+    super.initState();
+    headers = [0, 7, 25, 51, 100, 150, 498, 700, 5908];
+    items = List<Object>.generate(
+      10000,
+      (int index) => headers.contains(index) ? 'header $index' : index,
+    );
+    gridViewItemsType = items
+        .map(
+          (e) => e is String ? GridViewItemType.header : GridViewItemType.item,
+        )
+        .toList();
+    gridDelegate = CustomGridDelegate(
+      itemMaxCrossAxisExtent: 240.0,
+      itemMainAxisExtent: 40.0,
+      itemCrossAxisStacing: 12.0,
+      itemMainAxisStacing: 12.0,
+      titleMainAxisExtent: 80.0,
+      titleMainAxisBeforeSpacing: 15.0,
+      titleMainAxisAfterSpacing: 5.0,
+      itemTypes: gridViewItemsType,
+    );
+    scrollController = ScrollController();
+  }
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.refresh),
+          onPressed: () {
+            if (gridDelegate.geometries != null) {
+              final SliverGridGeometry geometry =
+                  gridDelegate.geometries![5908];
+              scrollController.animateTo(
+                geometry.scrollOffset,
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+            }
+          },
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Card(
+            elevation: 8.0,
+            child: GridView.builder(
+              controller: scrollController,
+              padding: const EdgeInsets.all(12.0),
+              gridDelegate: gridDelegate,
+              itemCount: items.length,
+              itemBuilder: (BuildContext context, int index) {
+                if (index > items.length - 1) return null;
+                final Object item = items[index];
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+                return Container(
+                  decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0),
+                    ),
+                    gradient: const RadialGradient(
+                      colors: <Color>[Color(0x0F88EEFF), Color(0x2F0099BB)],
+                    ),
+                  ),
+                  child: item is String
+                      ? Center(child: Text(item))
+                      : Center(child: Text('Item $index')),
+                );
+              },
             ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+enum GridViewItemType { header, item }
+
+class CustomGridDelegate extends SliverGridDelegate {
+  CustomGridDelegate({
+    required this.itemMaxCrossAxisExtent,
+    required this.itemMainAxisExtent,
+    required this.itemCrossAxisStacing,
+    required this.itemMainAxisStacing,
+    required this.titleMainAxisExtent,
+    required this.titleMainAxisBeforeSpacing,
+    required this.titleMainAxisAfterSpacing,
+    required this.itemTypes,
+  });
+
+  final double itemMaxCrossAxisExtent;
+  final double itemMainAxisExtent;
+  final double itemCrossAxisStacing;
+  final double itemMainAxisStacing;
+  final double titleMainAxisExtent;
+  final double titleMainAxisBeforeSpacing;
+  final double titleMainAxisAfterSpacing;
+  final List<GridViewItemType> itemTypes;
+  List<SliverGridGeometry>? geometries;
+
+  @override
+  SliverGridLayout getLayout(SliverConstraints constraints) {
+    int itemCrossAxisCount =
+        (constraints.crossAxisExtent /
+                (itemMaxCrossAxisExtent + itemCrossAxisStacing))
+            .ceil();
+    // Ensure a minimum count of 1, can be zero and result in an infinite extent
+    // below when the window size is 0.
+    itemCrossAxisCount = math.max(1, itemCrossAxisCount);
+    final double usableCrossAxisExtent = math.max(
+      0.0,
+      constraints.crossAxisExtent -
+          itemCrossAxisStacing * (itemCrossAxisCount - 1),
+    );
+    final double evaluatedItemsCrossAxisExtent =
+        usableCrossAxisExtent / itemCrossAxisCount;
+    final List<SliverGridGeometry> localGeometries = <SliverGridGeometry>[];
+    int indexInRow = 0;
+
+    double currentMainAxisOffset = 0.0;
+    double currentCrossAxisOffset = 0.0;
+    for (int i = 0; i < itemTypes.length; i++) {
+      final GridViewItemType item = itemTypes[i];
+      switch (item) {
+        case GridViewItemType.header:
+          indexInRow = 0;
+          currentCrossAxisOffset = 0.0;
+          localGeometries.add(
+            SliverGridGeometry(
+              scrollOffset: currentMainAxisOffset, // "y"
+              crossAxisOffset: currentCrossAxisOffset, // "x"
+              mainAxisExtent: titleMainAxisExtent, // "height"
+              crossAxisExtent: constraints.crossAxisExtent, // "width"
+            ),
+          );
+          currentMainAxisOffset +=
+              titleMainAxisExtent + titleMainAxisAfterSpacing;
+          break;
+        case GridViewItemType.item:
+          if (indexInRow == itemCrossAxisCount) {
+            // start new row
+            indexInRow = 0;
+            currentCrossAxisOffset = 0.0;
+            currentMainAxisOffset += itemMainAxisExtent + itemMainAxisStacing;
+          }
+
+          localGeometries.add(
+            SliverGridGeometry(
+              scrollOffset: currentMainAxisOffset, // "y"
+              crossAxisOffset: currentCrossAxisOffset, // "x"
+              mainAxisExtent: itemMainAxisExtent, // "height"
+              crossAxisExtent: evaluatedItemsCrossAxisExtent, // "width"
+            ),
+          );
+
+          if ((i + 1 < itemTypes.length) &&
+              (itemTypes[i + 1] == GridViewItemType.header)) {
+            // end of section. add spacing
+            currentMainAxisOffset +=
+                itemMainAxisExtent + titleMainAxisBeforeSpacing;
+          } else {
+            currentCrossAxisOffset +=
+                itemCrossAxisStacing + evaluatedItemsCrossAxisExtent;
+            indexInRow++;
+          }
+          break;
+      }
+    }
+
+    geometries = localGeometries;
+    return CustomGridLayout(
+      crossAxisItemsCount: itemCrossAxisCount,
+      itemCrossAxisExtent: evaluatedItemsCrossAxisExtent,
+      titleCrossAxisExtent: constraints.crossAxisExtent,
+      geometries: localGeometries,
+      titleMainAxisExtentExtent: titleMainAxisExtent,
+      itemMainAxisExtentExtent: itemMainAxisExtent,
+    );
+  }
+
+  @override
+  bool shouldRelayout(CustomGridDelegate oldDelegate) {
+    return oldDelegate.itemMaxCrossAxisExtent != itemMaxCrossAxisExtent ||
+        oldDelegate.itemMainAxisExtent != itemMainAxisExtent ||
+        oldDelegate.titleMainAxisExtent != titleMainAxisExtent ||
+        oldDelegate.itemCrossAxisStacing != itemCrossAxisStacing ||
+        oldDelegate.itemMainAxisStacing != itemMainAxisStacing ||
+        oldDelegate.itemTypes != itemTypes;
+  }
+}
+
+class CustomGridLayout extends SliverGridLayout {
+  const CustomGridLayout({
+    required this.crossAxisItemsCount,
+    required this.itemCrossAxisExtent,
+    required this.titleCrossAxisExtent,
+    required this.titleMainAxisExtentExtent,
+    required this.itemMainAxisExtentExtent,
+    required this.geometries,
+  }) : assert(crossAxisItemsCount > 0);
+
+  final int crossAxisItemsCount;
+  final double itemCrossAxisExtent;
+  final double titleCrossAxisExtent;
+  final double titleMainAxisExtentExtent;
+  final double itemMainAxisExtentExtent;
+  final List<SliverGridGeometry> geometries;
+
+  @override
+  double computeMaxScrollOffset(int childCount) {
+    final SliverGridGeometry geometry = geometries[childCount - 1];
+    return geometry.scrollOffset + geometry.mainAxisExtent;
+  }
+
+  @override
+  SliverGridGeometry getGeometryForChildIndex(int index) {
+    return geometries[index];
+  }
+
+  @override
+  int getMinChildIndexForScrollOffset(double scrollOffset) {
+    final int result = geometries.indexWhere(
+      (SliverGridGeometry element) => element.scrollOffset >= scrollOffset,
+    );
+    return result;
+  }
+
+  @override
+  int getMaxChildIndexForScrollOffset(double scrollOffset) {
+    double greaterScrollOffset = 0.0;
+    int result = 0;
+    for (int i = 0; i < geometries.length; i++) {
+      final double itemOffset = geometries[i].scrollOffset;
+
+      if (scrollOffset > itemOffset) {
+        continue;
+      }
+      if (result == 0) {
+        greaterScrollOffset = itemOffset;
+        result = i;
+        continue;
+      }
+      if (greaterScrollOffset == itemOffset) {
+        //next item in same line
+        // update result
+        result = i;
+        continue;
+      }
+
+      // next line started
+      // return last index from previous line
+      break;
+    }
+    return result > 0 ? result : geometries.length - 1;
   }
 }
